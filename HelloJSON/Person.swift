33cb25bca3748820acff2a7f7f7280a98ab57227
TeChengHung <dan12411@gmail.com>
2016-10-14 16:45:42 +0800

    Build Struct Person

---
+++ HelloJSON/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  HelloJSON
+//
+//  Created by 洪德晟 on 2016/10/14.
+//  Copyright © 2016年 洪德晟. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    var name: String?
+    var email: String?
+    var number: String?
+    var photo: String?
+}
